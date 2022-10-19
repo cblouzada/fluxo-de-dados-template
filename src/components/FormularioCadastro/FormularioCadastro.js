ea3b99a5f8d431799b1162f6db15d726373a110b
Carlos Bergson <104369215+cblouzada@users.noreply.github.com>
2022-10-19 15:43:09 -0300

    Revert "Aula de fluxo de dados"

--- src/components/FormularioCadastro/FormularioCadastro.js
+++ src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,23 +1,6 @@
 import { FormContainer, Form, Input, StyledLabel } from "./styled";
-import { useState } from "react";
 
-export const FormularioCadastro = (props) => {
-
-  const [urlFoto, setUrlFoto] = useState("")
-
-  const [descricao, setDescricao] = useState ("")
-
-  function onChangeUrl(e){
-    props.setUrlFoto(e.target.value)
-  }
-
-  function onChangeDescricao(e){
-    props.setDescricao(e.target.value)
-  }
-
-  const onChangeTitulo =(e) =>{
-    props.setTitulo(e.target.value)
-  }
+export const FormularioCadastro = () => {
 
   return (
     <FormContainer>
@@ -25,15 +8,11 @@ export const FormularioCadastro = (props) => {
       <Form>
         <StyledLabel htmlFor="foto">
           Imagem:
-          <Input id="foto" value={props.urlFoto} onChange={onChangeUrl} />
+          <Input id="foto" />
         </StyledLabel>
         <StyledLabel htmlFor="descricao">
           Descrição:
-          <Input id="descricao" value={props.descricao} onChange={onChangeDescricao} />
-        </StyledLabel>
-        <StyledLabel htmlFor="titulo">
-          Titulo do post:
-          <Input id="titulo" value={props.titulo} onChange={onChangeTitulo} />
+          <Input id="descricao" />
         </StyledLabel>
       </Form>
     </FormContainer>
